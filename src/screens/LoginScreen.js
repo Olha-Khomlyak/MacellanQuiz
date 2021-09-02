ddb5e70b543ac98c10e31a4e3a836703d1967f4c
Olha Khomlyak <olga.khomlyak@gmail.com>
2021-09-02 18:47:13 +0300

    navigation setup

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+ const LoginScreen = () => {
+     return (
+         <View style={{flex:1, backgroundColor: 'green'}}>
+             <Text>login</Text>
+         </View>
+     );
+ }
+ 
+ export default LoginScreen;
