62ec9b27959232f324ac70003e9385b440b1513e
Olha Khomlyak <olga.khomlyak@gmail.com>
2021-09-16 12:11:22 +0300

    drawer navigation created

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,40 +1,76 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, CommonActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import { createDrawerNavigator, DrawerItem, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import LoginScreen from '../screens/LoginScreen'
 import HomeScreen from '../screens/HomeScreen'
 import { styles } from '../constants/styles';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const AuthNavigator = () => {
+    return (
+        <Stack.Navigator
+            initialRouteName="Login"
+        >
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{
+                    title: 'Giriş Yapın',
+                    headerTitleStyle: styles.headerTitleStyle,
+                    headerTitleAlign: 'center'
+                }}
+            />
+            <Stack.Screen
+                name="Home"
+                component={DrawerNavigator}
+                options={{
+                   headerShown: false
+                }}
+            />
+        </Stack.Navigator>
+    )
+}
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator drawerContent={props => {
+            return (
+                <DrawerContentScrollView {...props}>
+                    <DrawerItemList {...props} />
+                    <DrawerItem label="Logout" onPress={() => 
+                      {  props.navigation.dispatch({
+                            ...CommonActions.reset({
+                                index: 0,
+                                routes: [
+                                    { name: 'Login' },
+                                ]
+                            })
+                        })}
+                    }/>
+                </DrawerContentScrollView>
+            )
+        }}>
+            <Drawer.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    title: 'Cüzdanlar',
+                    headerTitleStyle: styles.headerTitleStyle,
+                    headerTitleAlign: 'center'
+
+                }} />
+        </Drawer.Navigator>
+    );
+}
 
 const Navigator = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                initialRouteName="Login"
-            >
-                <Stack.Screen
-                    name="Login"
-                    component={LoginScreen}
-                    options={{
-                        title: 'Giriş Yapın',
-                        headerTitleStyle: styles.headerTitleStyle,
-                        headerTitleAlign: 'center'
-                    }}
-                />
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        title: 'Cüzdanlar',
-                        headerTitleStyle: styles.headerTitleStyle,
-                        headerTitleAlign: 'center'
-                        
-                    }}
-                />
-            </Stack.Navigator>
+            <AuthNavigator />
         </NavigationContainer>
     );
 }
