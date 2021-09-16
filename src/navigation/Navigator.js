66d1e8f7d3a46661639b7ff8aa48d6c4afa7ad14
Olha Khomlyak <olga.khomlyak@gmail.com>
2021-09-16 14:19:24 +0300

    Home header fixed

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { View } from 'react-native';
 import { NavigationContainer, CommonActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator, DrawerItem, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import LoginScreen from '../screens/LoginScreen'
 import HomeScreen from '../screens/HomeScreen'
 import { styles } from '../constants/styles';
+import { Icon, Badge } from 'react-native-elements'
+import { colors } from '../constants';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -28,7 +31,7 @@ const AuthNavigator = () => {
                 name="Home"
                 component={DrawerNavigator}
                 options={{
-                   headerShown: false
+                    headerShown: false
                 }}
             />
         </Stack.Navigator>
@@ -37,32 +40,58 @@ const AuthNavigator = () => {
 
 const DrawerNavigator = () => {
     return (
-        <Drawer.Navigator drawerContent={props => {
-            return (
-                <DrawerContentScrollView {...props}>
-                    <DrawerItemList {...props} />
-                    <DrawerItem label="Logout" onPress={() => 
-                      {  props.navigation.dispatch({
-                            ...CommonActions.reset({
-                                index: 0,
-                                routes: [
-                                    { name: 'Login' },
-                                ]
+        <Drawer.Navigator
+
+            drawerContent={props => {
+                return (
+                    <DrawerContentScrollView {...props}>
+                        <DrawerItemList {...props}
+                        />
+                        <DrawerItem label="Logout" onPress={() => {
+                            props.navigation.dispatch({
+                                ...CommonActions.reset({
+                                    index: 0,
+                                    routes: [
+                                        { name: 'Login' },
+                                    ]
+                                })
                             })
-                        })}
-                    }/>
-                </DrawerContentScrollView>
-            )
-        }}>
+                        }
+                        } />
+                    </DrawerContentScrollView>
+                )
+            }}>
             <Drawer.Screen
                 name="Home"
                 component={HomeScreen}
                 options={{
                     title: 'Cüzdanlar',
                     headerTitleStyle: styles.headerTitleStyle,
-                    headerTitleAlign: 'center'
+                    headerTitleAlign: 'center',
+                    headerTintColor: colors.PURPLE,
+                    drawerActiveTintColor: colors.PURPLE,
+                    headerRight: () => {
+                        return (
+                            <View>
+                                <Icon
+                                    type="font-awesome-5"
+                                    name='bell'
+                                    solid
+                                    size={20}
+                                    color={colors.PURPLE}
+                                    iconStyle={{ marginRight: 10 }}
+                                />
+                                <Badge
+                                    status="error"
+                                    containerStyle={{ position: 'absolute', bottom: 3, left: -3 }}
+                                    badgeStyle={{ backgroundColor: colors.ORANGE }}
+                                />
+                            </View>
+                        )
+                    }
 
-                }} />
+                }}
+            />
         </Drawer.Navigator>
     );
 }
